1acdab4cba50fd2232d7f125e4c0fb6ba3085c0b
roman <aboandrus@gmail.com>
2019-10-06 19:17:20 +0200

    Refactor code

--- src/main/java/Dinglemouse.java
+++ src/main/java/Dinglemouse.java
@@ -1,9 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Dinglemouse {
 
     public static String histogram(final int tempults[]) {
@@ -11,28 +5,20 @@ public class Dinglemouse {
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < tempults.length; i++) {
             StringBuilder temp = new StringBuilder();
-            temp.append(String.valueOf(i + 1)).append("|");
-            StringBuilder line = new StringBuilder();
-
+            temp.append(String.valueOf(i + 1 +"|"));
             if (tempults[i] != 0) {
+                StringBuilder line = new StringBuilder();
                 for (int j = 0; j < tempults[i] / 2; j++) {
                     line.append(as);
                 }
-                temp.append(line).append(" " + String.valueOf(tempults[i])).append("%\n");
-            }else{
-                temp.append("\n");
+                temp.append(line+" " + String.valueOf(tempults[i])).append("%");
             }
-
+            temp.append("\n");
             res.insert(0, temp);
         }
         return res.toString();
     }
 
-
-    public static void main(String[] args) {
-        int tempults[] = {2, 15, 9, 0, 7};
-        System.out.println(histogram(tempults));
-    }
 
 
 }
